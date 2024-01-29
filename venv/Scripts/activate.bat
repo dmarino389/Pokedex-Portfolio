83cebee4b0f28f1f9f9894724870ee357cc1a8e2
dmarino389 <139820583+dmarino389@users.noreply.github.com>
2024-01-28 23:44:27 -0600

    done

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\marin\OneDrive\Desktop\Applications\Need To Deploy\Pokehw\venv
+set VIRTUAL_ENV=C:\Users\marin\OneDrive\Desktop\Pokehw\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
