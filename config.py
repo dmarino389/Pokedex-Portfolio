3575e3078a6db9fcdf987d85eb460be2106812ff
dmarino389 <139820583+dmarino389@users.noreply.github.com>
2024-02-01 19:14:00 -0600

    done

--- config.py
+++ config.py
@@ -2,11 +2,10 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-class Config():
-  FLASK_APP = os.environ.get("FLASK_APP")
-  FLASK_DEBUG = os.environ.get("FLASK_DEBUG")
-  SECRET_KEY = os.environ.get('SECRET_KEY')
-  SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
-  SQLALCHEMY_TRACK_MODIFICATIONS = False
-  
+class Config:
+    FLASK_APP = os.environ.get("FLASK_APP", "run.py")
+    FLASK_DEBUG = os.environ.get("FLASK_DEBUG", 1)
+    SECRET_KEY = os.environ.get('SECRET_KEY', '1234')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
